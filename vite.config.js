0b1aac95d8968c980c807cbd5db642c71adbb221
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-28 09:05:16 +0800

    removed fs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const fs = require('fs')
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    https: {
-      key: fs.readFileSync('/etc/ssl/private/my.key'),
-      cert: fs.readFileSync('/etc/ssl/certs/my.crt'),
-    },
-  },
 })
